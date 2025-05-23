07087659c4c79cbd2e4da1a21ef5a2d686a8f45d
Casey Idzikowski <65414051+Idzikowski-Casey@users.noreply.github.com>
2025-05-23 08:21:29 -0400

    search for data (#2)

--- audio/src/main/java/com/application/audio/FileHelper.kt
+++ audio/src/main/java/com/application/audio/FileHelper.kt
@@ -14,9 +14,7 @@ class FileHelper {
         val byteBuffer = inputStream.readBytes()
         inputStream.close()
 
-        // Basic WAV parsing: skip first 44 bytes (WAV header)
-        val headerSize = 44
-        val pcmData = byteBuffer.copyOfRange(headerSize, byteBuffer.size)
+        val pcmData = parseDataChunk(byteBuffer)
 
         val floatList = mutableListOf<Float>()
         val byteBufferWrap = ByteBuffer.wrap(pcmData)
@@ -28,5 +26,38 @@ class FileHelper {
         }
 
         return floatList.toFloatArray()
+    }
+
+    /**
+     * Search through byte array for the 'data' chunk in the array. Grab the
+     * data content and return it. This will be the pure PCM data filtered of any other
+     * metadata.
+     */
+    private fun parseDataChunk(wavBytes: ByteArray): ByteArray {
+        var dataChunk = byteArrayOf()
+
+        // skip over RIFF header of 12 bytes
+        var offset = 12
+
+        while (offset + 8 <= wavBytes.size) {
+            val chunkId = String(wavBytes, offset, 4)
+            val chunkSize = ByteBuffer.wrap(wavBytes, offset + 4, 4)
+                .order(ByteOrder.LITTLE_ENDIAN).int
+
+            val chunkDataStart = offset + 8
+            val chunkDataEnd = chunkDataStart + chunkSize
+
+            if (chunkDataEnd > wavBytes.size) break
+
+            val content = wavBytes.copyOfRange(chunkDataStart, chunkDataEnd)
+
+            if (chunkId == "data") {
+                dataChunk += content
+            }
+
+            offset += 8 + chunkSize
+        }
+
+        return dataChunk
     }
 }
