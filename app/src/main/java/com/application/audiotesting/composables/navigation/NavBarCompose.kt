30a37fc1683167da19156ffc172b1f6715ceefc8
Casey Idzikowski <idzikoj1234@gmail.com>
2025-06-02 08:26:28 -0400

    small clean up

--- app/src/main/java/com/application/audiotesting/composables/navigation/NavBarCompose.kt
+++ app/src/main/java/com/application/audiotesting/composables/navigation/NavBarCompose.kt
@@ -1,10 +1,7 @@
 package com.application.audiotesting.composables.navigation
 
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,22 +13,12 @@ import com.application.audiotesting.ui.theme.AudioTestingTheme
 
 @Composable
 fun NavBarCompose(tabs: List<Tab>) {
-    CompositionLocalProvider(
-        LocalContentColor provides Color.Unspecified
+    NavigationBar(
+        containerColor = Color.White.copy(alpha = 0.5f),
+        tonalElevation = 0.dp
     ) {
-        Surface(
-            color = Color.Transparent,
-            tonalElevation = 0.dp,
-            shadowElevation = 0.dp
-        ) {
-            NavigationBar(
-                containerColor = Color.Transparent,
-                tonalElevation = 0.dp
-            ) {
-                tabs.map {
-                    TabNavigationItem(tab = it)
-                }
-            }
+        tabs.map {
+            TabNavigationItem(tab = it)
         }
     }
 }
